c802d5799eee121dbcc733070b9ef06d710b5453
Sergejs Basangovs <sergejs.basangovs@gmail.com>
2023-01-17 20:35:26 +0200

    feat: sale details page - ssr prefetch query

--- src/pages/sales/[id].tsx
+++ src/pages/sales/[id].tsx
@@ -1,23 +1,67 @@
-import React from 'react';
-import type { NextPage } from 'next'
-import Head from 'next/head'
+import React from "react";
+import type { NextPage, GetServerSideProps } from "next";
+import Head from "next/head";
 import { useRouter } from "next/router";
+import { useQuery } from "@apollo/client";
 
-type PageProps = {}
+import {
+  GET_SALE_BY_ID,
+  initialiseApolloClient,
+  addApolloState,
+} from "@/graphql";
+import { SaleDetails } from "@/types";
 
-const SaleDetailsPage: NextPage<PageProps> = ({}) => {
-  const router = useRouter()
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  const saleId = params?.id as string;
+  const apolloClient = initialiseApolloClient();
+  const { data } = await apolloClient.query<{ sale: SaleDetails }>({
+    query: GET_SALE_BY_ID,
+    variables: {
+      saleId,
+    },
+  });
+
+  return addApolloState(apolloClient, {
+    props: {
+      pageTitle: data.sale.editorial.title,
+      metaDescription: data.sale.editorial.destinationName,
+    },
+  });
+};
+
+type PageProps = {
+  pageTitle?: string;
+  metaDescription?: string;
+};
+
+const SaleDetailsPage: NextPage<PageProps> = ({
+  pageTitle,
+  metaDescription,
+}) => {
+  const router = useRouter();
+  const { data, loading, error } = useQuery<{ sale: SaleDetails }>(
+    GET_SALE_BY_ID,
+    {
+      variables: { saleId: router.query.id },
+    }
+  );
+
+  console.log({ data, loading, error });
+
   return (
     <>
       <Head>
-        <title>Sale Page</title>
-        <meta name='description' content='Sale details page' />
+        <title>{pageTitle || "Sale Details Page"}</title>
+        <meta
+          name="description"
+          content={metaDescription || "Sale details page"}
+        />
       </Head>
 
       <h1>Sale Details Page</h1>
       <p>Sale ID: {router.query.id}</p>
     </>
-  )
-}
+  );
+};
 
-export default SaleDetailsPage
+export default SaleDetailsPage;
